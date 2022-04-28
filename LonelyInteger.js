9a3d0ee7dddf2c108ec3d0d3c37d5d92a068b59f
sabdahtb <sabdahutabarat2804@gmail.com>
2022-04-28 10:37:06 +0700

    sole lonelyinteger

--- LonelyInteger.js
+++ LonelyInteger.js
@@ -1,10 +1,16 @@
+// Mencari value dari array yang jumlahnya = 1
+// dari 1 2 3 4 3 2 1 yang jumlah value nya = 1 adalah 4
+
 const arr = [1, 2, 3, 4, 3, 2, 1];
 
 function lonelyInteger(arr) {
   let sortArr = arr.sort();
+  // urutkan array menggunakan array.sort()
+  // [1,2,3,4,3,2,1] akan menjadi [1,1,2,2,3,3,4]
   let lonelyint = sortArr.filter(
     (x, i, arr) => x !== arr[i + 1] && x !== arr[i - 1]
   );
+  // filter array yang sudah di sort dimana tidak ada value sama di arr[i] sebelum dan sesudah
   return lonelyint;
 }
 
